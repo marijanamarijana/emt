8528926006350f68375b7d43836344320568a3da
marijanamarijana <marijana.petrovska@students.finki.ukim.mk>
2025-03-30 00:10:40 +0100

    Adding lab2 - files

--- src/main/java/com/example/emtlab/dto/BookDisplayDto.java
+++ src/main/java/com/example/emtlab/dto/BookDisplayDto.java
@@ -1,6 +1,5 @@
 package com.example.emtlab.dto;
 
-import com.example.emtlab.model.domain.Author;
 import com.example.emtlab.model.domain.Book;
 import com.example.emtlab.model.enumeration.BookCategory;
 
